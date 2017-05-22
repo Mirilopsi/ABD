9bcedc016220848fc9b7cfaed4b24a41d600a0ba
Miriam López Sierra <lopezsierramiriam@gmail.com>
2017-05-22 23:30:15 +0200

    Separado las partes comunes de todas las páginas

---
+++ Views/Common/nav.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="http://localhost/practicaABD/Styles/nav.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC|Barrio|Open+Sans+Condensed:300" rel="stylesheet">    
+    <link href="http://localhost/practicaABD/Styles/main.css" rel="stylesheet">
+    <link rel="icon" type="image/png" sizes="32x32" href="Assets/logo/favicon.png">
+
+
+</head>
+
+<body>
+    <nav id="menu">
+        <div class="navBar">
+            <li>
+                <a href= 'http://localhost/practicaABD/index.php'><img src="http://localhost/practicaABD/Assets/logo/logo2.png"></a>
+            </li>
+            <ul class="botonesMenu">
+                <li class="opcion parent">Mensajes
+                    <ul>
+                        <li><a href="http://localhost/practicaABD/Views/MensajesGlobales.php">Globales</a></li>
+                        <li>Personales</li>
+                    </ul>
+                </li>
+                <li class="opcion">Mi Perfil</li>
+                <li class="opcion">LogIn</li>
+            </ul>
+
+        </div>
+    </nav>
+
+</body>
